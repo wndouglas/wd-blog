bfceaae6688be079a1c495caf957555483e4d028
wndouglas <wndouglas@hotmail.co.uk>
2020-05-05 17:42:27 +0100

    Fixed logo

--- src/utils/typography.js
+++ src/utils/typography.js
@@ -1,7 +1,9 @@
 import Typography from "typography"
 import KirkhamTheme from "typography-theme-kirkham"
 
-KirkhamTheme.headerFontFamily = ['Roboto']
+KirkhamTheme.bodyFontFamily = ["Georgia", "serif"]
+KirkhamTheme.headerFontFamily = ["Roboto", "Avenir Next", "Helvetica Neue", "Segoe UI", "Helvetica", 
+    "Arial", "sans-serif"]
 const typography = new Typography(KirkhamTheme)
 
 export default typography
