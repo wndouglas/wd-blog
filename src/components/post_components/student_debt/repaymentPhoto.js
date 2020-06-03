a47bc5bfc582f6bbb3779cf87cf3f782d395b3b6
wndouglas <wndouglas@hotmail.co.uk>
2020-06-03 21:30:10 +0100

    Added conclusion

--- src/components/post_components/student_debt/repaymentPhoto.js
+++ src/components/post_components/student_debt/repaymentPhoto.js
@@ -3,11 +3,11 @@ import React from 'react'
 import Img from "gatsby-image"
 import { Container, Col } from "react-bootstrap"
 
-export default ( ) => {
-    const { rpi } = useStaticQuery(
+const RepaymentPlots = ( ) => {
+    const { wealth } = useStaticQuery(
         graphql`
           query {
-            rpi: file(relativePath: { eq: "post-plots/rpi.png" }) {
+            wealth: file(relativePath: { eq: "post-plots/wealth.png" }) {
               childImageSharp {
                 fluid(quality: 100) {
                   ...GatsbyImageSharpFluid_withWebp
@@ -20,9 +20,11 @@ export default ( ) => {
     return (
       <Container align='center'>
         <Col xs={10} md={10}>
-          <Img fluid={rpi.childImageSharp.fluid} />
+          <Img fluid={wealth.childImageSharp.fluid} />
         </Col>
         <br/>
       </Container>
     )
-}
+  }
+
+export default RepaymentPlots
---
+++ src/components/post_components/student_debt/rpiPhoto.js
@@ -0,0 +1,30 @@
+import { graphql, useStaticQuery } from 'gatsby'
+import React from 'react'
+import Img from "gatsby-image"
+import { Container, Col } from "react-bootstrap"
+
+const RPI = () => {
+    const { rpi } = useStaticQuery(
+        graphql`
+          query {
+            rpi: file(relativePath: { eq: "post-plots/rpi.png" }) {
+              childImageSharp {
+                fluid(quality: 100) {
+                  ...GatsbyImageSharpFluid_withWebp
+                }
+              }
+            }
+          }
+        `
+      )
+    return (
+      <Container align='center'>
+        <Col xs={10} md={10}>
+          <Img fluid={rpi.childImageSharp.fluid} />
+        </Col>
+        <br/>
+      </Container>
+    )
+}
+
+export default RPI
